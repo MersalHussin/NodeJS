6ba61da398eeaff005714f27aa0dc93b2b911572
MersalHussin <mersalhussin@gmai.com>
2026-05-05 04:26:55 +0300

    Header Content Type

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@
 import * as http from 'http'
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Hello, World!")
+    res.writeHead(200, { "Content-Type": "text/html" }) // == We change contet-type : html / json / ...etc
+    res.write("<h1>Hello, World!</h1>") // => to write something
+    // res.end("Hello, World!")  ==> Important for End Server 
 })
 
 const PORT = 8000
