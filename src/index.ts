61f516bbaae558210d5f40d19eb0fd412b851aed
MersalHussin <mersalhussin@gmai.com>
2026-05-05 19:04:01 +0300

    Rendring Add Product Route (JUST UI)

--- src/index.ts
+++ src/index.ts
@@ -21,17 +21,21 @@ if(req.url === '/proudcts' && req.method === 'GET'){
     res.writeHead(200, { "Content-Type": "text/html" })
     res.write(`<h1>Hello From Server</h1>`)
     res.end()
-}else if (req.url === '/proudcts/new'){
+}else if (req.url === '/proudcts/new' && req.method === 'GET'){
     res.writeHead(200, { "Content-Type": "text/html" })
     res.write(`
-        <form method="POST action="./add-proudct">
-            <input type="text" name="name" placeholder="Enter Name" />
-            <input type="number" name="age" placeholder="Enter Age" />
-            <button type="submit">Send</button>
+        <form method="POST" action="/add-proudct">
+        <input type="text" name="name" placeholder="Enter Name" />
+        <input type="number" name="age" placeholder="Enter Age" />
+        <button type="submit">Send</button>
         </form>
         `)
-        res.end()
-}else{
+    }else if (req.url === '/add-proudct' && req.method === "POST"){
+        res.writeHead(200, {"Content-Type": "text/html" })
+        res.end('add a New Product')
+    }
+
+else{
     res.writeHead(404, { "Content-Type": "text/html" })
     res.write(`<h1>404 Page Not Found</h1>`)
     res.end()
