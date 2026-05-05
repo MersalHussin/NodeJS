4236180d21e2249497ca5fe052448e514dbcb3f1
MersalHussin <mersalhussin@gmai.com>
2026-05-06 01:56:01 +0300

    Dealing With File System

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 // Local Server 
+import { readFile } from 'fs'
 import * as http from 'http'
 
 const server = http.createServer((req, res) => {
 
 if(req.url === '/proudcts' && req.method === 'GET'){
 
-    const data = [        
-        { name: "Riad", age: 23 },
-        { name: "Adam", age: 33 },
-        { name: "Sally", age: 29 },
-        { name: "aSalsly", age: 219 },
-        { name: "aSally", age: 29 },
-        { name: "aSally", age: 2922212 },
-        { name: "aSally", age: 2922 }
-    ]
      res.writeHead(200, { "Content-Type": "application/json" }) // == We change contet-type : html / json / ...etc
-     res.write(JSON.stringify(data)) // => to write something
+     readFile("./src/data/users.json", ((err,data) =>{
+        if(err){
+            console.error("Error =>",err);
+        }else{
+            console.error("Data =>",data.toString());
+        }
+    } ))
      res.end("\n")  //==>  Important for End Server 
 }else if (req.url === '/' && req.method === 'GET'){
     res.writeHead(200, { "Content-Type": "text/html" })
