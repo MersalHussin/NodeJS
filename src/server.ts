48c87c07353057691aa2ca718420a4fdeb797523
MersalHussin <mersalhussin@gmai.com>
2026-05-07 02:26:36 +0300

    make Products with Prameters

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,26 @@ app.get("/" , (req,res)=>{
     res.send("Hello in Expreess.js")
 })
 
+const data = [
+    {
+        id :"1",
+        name :"Hello",
+    },
+    {
+        id :"2",
+        name :"Hello2",
+    },
+    {
+        id :"3",
+        name :"Hello2",
+    },
+]
+app.get("/proudcts",(req,res)=>{
+    res.send(data)})
+app.get("/proudcts/:id",(req,res)=>{
+    res.send(data.filter(product => product.id === req.params.id))
+})
+
 app.listen(PORT,() =>{
     console.log(`Server is Opend at => http://localhost:${PORT}`);
 })
