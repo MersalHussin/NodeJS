7aa8f2cde63878d3b762308d313b6d0b626b986e
MersalHussin <mersalhussin@gmai.com>
2026-05-07 03:42:33 +0300

    Generate Fake Data

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { generateFakeProducts } from './utils/fakeData.ts';
 
 const app = express()
 const PORT:number = 8000;
@@ -8,24 +9,11 @@ app.get("/" , (req,res)=>{
     res.send("Hello in Expreess.js")
 })
 
-const data = [
-    {
-        id :"1",
-        name :"Hello",
-    },
-    {
-        id :"2",
-        name :"Hello2",
-    },
-    {
-        id :"3",
-        name :"Hello2",
-    },
-]
+const data = generateFakeProducts()
 app.get("/proudcts",(req,res)=>{
     res.send(data)})
-app.get("/proudcts/:id",(req,res)=>{
-    res.send(data.filter(product => product.id === req.params.id))
+    app.get("/proudcts/:id",(req,res)=>{
+    res.send(data.filter(product => product.id === +req.params.id))
 })
 
 app.listen(PORT,() =>{
